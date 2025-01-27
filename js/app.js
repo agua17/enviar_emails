3f46c792d9507306875bf8ef64e14afc3f41d377
Agua <aguassantasnunez@gmail.com>
2025-01-27 06:24:40 +0100

    mensaje_alerta

--- js/app.js
+++ js/app.js
@@ -2,15 +2,27 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     inputEmail.addEventListener('blur', validar);
 
     inputAsunto.addEventListener('blur', validar);
 
-    inputMensaje.addEventListener('blur', validar)
+    inputMensaje.addEventListener('blur', validar);
 
     function validar(evt){
-        console.log(evt.target.value);
+        if(evt.target.value.trim()=== ''){
+            mostrarAlerta(`El campo ${evt.target.id} es obligatorio`);
+        }else{
+            console.log('Si hay algo');
+        }
+    }
+
+    function mostrarAlerta(mensaje){
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        formulario.appendChild(error);
     }
 });
 
