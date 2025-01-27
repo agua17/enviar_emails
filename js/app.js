132cccb52d5c8757c72caf7be97cef8cba16bd64
Agua <aguassantasnunez@gmail.com>
2025-01-27 05:47:59 +0100

    crecion_variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-document.addEventListener('DOMContentLoaded', function(){})
+document.addEventListener('DOMContentLoaded', function(){
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+});
+
+inputEmail.addEventListener('blur', function(){
+    console.log('Se está ejecutando porque el usuario dejó de interactuar con el elemento');
+})
